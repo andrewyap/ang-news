89b89cb54f4b16e9097bc6860cc04def3a19ee13
Andrew Yap <andrew@yconsult.com.au>
2014-09-22 08:41:43 +1000

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angNewsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angNewsApp
+ */
+
+
+
+// #################### // 
+// # BASE YEOMAN CODE # //
+// #################### //
+
+// angular.module('angNewsApp')
+//   .controller('MainCtrl', function ($scope) {
+//     $scope.awesomeThings = [
+//       'HTML5 Boilerplate',
+//       'AngularJS',
+//       'Karma'
+//     ];
+//   });
